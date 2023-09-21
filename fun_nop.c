c690006ac2dd5625e0a132940c3b015228f02166
Omar Hady <ohady1992@gmail.com>
2023-09-21 16:42:50 -0700

    Initiate

---
+++ fun_nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+  *fun_nop-does nothing, no operation
+  *@head: head
+  *@counter: line #
+ */
+void fun_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
---
+++ stck.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * f_stack-prnts stack top
+ * @head: head
+ * @counter: line_number
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
